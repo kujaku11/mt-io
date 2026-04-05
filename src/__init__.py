337d50b2db98c3dcc6a4adb63ad7cd2519fafdb2
JP <peacock.jared@gmail.com>
2026-04-05 14:21:56 -0700

    moved files to src

--- src/__init__.py
+++ src/__init__.py
@@ -1,39 +1,6 @@
-"""Top-level package for mt-io."""
-
-# =============================================================================
-# Imports
-# =============================================================================
-import sys
-import xarray as xr
-from loguru import logger
-
-from mt_io.reader import read_file
-
-# =============================================================================
-# Package Variables
-# =============================================================================
-
-__author__ = """MT-IO Development Team"""
-__email__ = "jpeacock@usgs.gov"
-__version__ = "0.0.1"
+# package file
+from .collection import Collection
+from .reader import read_file
 
 
-# =============================================================================
-# Initialize Loggers
-# =============================================================================
-config = {
-    "handlers": [
-        {
-            "sink": sys.stdout,
-            "level": "INFO",
-            "colorize": True,
-            "format": "<level>{time} | {level: <3} | {name} | {function} | line: {line} | {message}</level>",
-        },
-    ],
-    "extra": {"user": "someone"},
-}
-logger.configure(**config)
-
-# need to set this to make sure attributes of data arrays and data sets
-# are kept when doing xarray computations like merge.
-xr.set_options(keep_attrs=True)
+__all__ = ["Collection", "read_file"]
--- src/timeseries/__init__.py
+++
@@ -1,4 +0,0 @@
-from .channel_ts import ChannelTS
-from .run_ts import RunTS
-
-__all__ = ["ChannelTS", "RunTS"]
